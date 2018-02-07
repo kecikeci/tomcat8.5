107ad679f5440a9e74d675fe315b0a799112828f
kecikeci <ceo@kecikeci.com>
2018-02-07 18:22:11 +0800

    kecikeci

---
+++ run.py
@@ -0,0 +1,13 @@
+# 启动时运行的脚本
+
+import warnings
+warnings.filterwarnings('ignore')
+
+import os
+
+#启动ssh
+os.system("/usr/sbin/sshd -D")
+#启动tomcat
+os.system("/data/apache-tomcat-8.5.27/bin/startup.sh")
+
+
