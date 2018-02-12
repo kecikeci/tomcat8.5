51dd2f6863ef4a98e912946023d89a0616a7b9e9
kecikeci <ceo@kecikeci.com>
2018-02-12 18:00:34 +0800

    update

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ print("分配给JVM的内存：%sM" % (tomcat_m))
 
 output=open('/etc/profile', 'a')
 is_opts = commands.getstatusoutput('cat /etc/profile|grep "JAVA_OPTS"|wc -l')
-if (is_opts[0]<1):
+if (int(is_opts[1])<1):
     output.write('export JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -server -Xms64m -Xmx'+str(tomcat_m)+'m -Dfile.encoding=UTF-8"')
 output.close()
 
