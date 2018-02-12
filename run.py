af7cfd6adbdc47ea885bf16d0d0b5f190836dccf
kecikeci <ceo@kecikeci.com>
2018-02-12 11:29:02 +0800

    init

--- run.py
+++ run.py
@@ -16,7 +16,9 @@ if(shell_r[1] and long(shell_r[1])!=9223372036854771712):
 print("分配给JVM的内存：%sM" % (tomcat_m))
 
 output=open('/etc/profile', 'a')
-output.write('export JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -server -Xms64m -Xmx'+str(tomcat_m)+'m -Dfile.encoding=UTF-8"')
+is_opts = commands.getstatusoutput('cat /etc/profile|grep "JAVA_OPTS"|wc -l')
+if (is_opts[0]<1):
+    output.write('export JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -server -Xms64m -Xmx'+str(tomcat_m)+'m -Dfile.encoding=UTF-8"')
 output.close()
 
 subprocess.call("source /etc/profile && exec /data/tomcat-8.5.27/bin/startup.sh", shell=True)
